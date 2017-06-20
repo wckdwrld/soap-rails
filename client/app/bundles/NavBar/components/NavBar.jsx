a1a24cca45ee1fce63170576a578ce433f8ae732
Jack Bennett <jack@Jacks-MacBook-Pro.local>
2017-06-20 15:58:34 +1000

    Video Added

--- client/app/bundles/NavBar/components/NavBar.jsx
+++ client/app/bundles/NavBar/components/NavBar.jsx
@@ -18,16 +18,29 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar color="faded" light toggleable>
+        <Navbar light toggleable>
           <NavbarToggler right onClick={this.toggle} />
           <NavbarBrand href="/">SOAP</NavbarBrand>
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
+             <NavItem>
+                <NavLink href="/concept">Concept</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                |
+                </NavLink>
+              </NavItem>
               <NavItem>
                 <NavLink href="/podcast">Podcast</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/portfolio">Hire Us</NavLink>
+                <NavLink>
+                |
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/portfolio">Services</NavLink>
               </NavItem>
             </Nav>
           </Collapse>
--- client/app/bundles/Portfolio/components/Portfolio.jsx
+++ client/app/bundles/Portfolio/components/Portfolio.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import InstagramEmbed from 'react-instagram-embed'
+import ReactPlayer from 'react-player'
+import {Button} from 'reactstrap'
+
 
 export default class Portfolio extends React.Component {
   
   render() {
     return (
+      <div>
+        <div className="jumbotron video-container">
+            <ReactPlayer url='https://vimeo.com/221771206' width="100%" height="90vh" />      
+        </div>
 
-      <InstagramEmbed url='https://www.instagram.com/p/BVYXe_AjJw5/?taken-by=browncardigan' />
+        <div className="jumbotron promotion-jumbo">
+          <h2>Promo</h2>
+            <a href=""> Learn More! </a>
+        </div>
 
+        <div className="jumbotron document-jumbo">
+        <h2>Document</h2>
+        </div>
+
+        <div className="jumbotron analytic-jumbo">
+        <h2>Analytics</h2>
+        </div>
+        
+      </div>
     );
   }
 }
