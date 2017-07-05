a6f23beb784fe9facd3e32e3e3f36420395ebf7a
Jack Bennett <jack@Jacks-MacBook-Pro.local>
2017-07-05 12:43:32 +1000

    Content-Image working

--- client/app/bundles/ContentBlock/components/ContentBlock.jsx
+++ client/app/bundles/ContentBlock/components/ContentBlock.jsx
@@ -11,13 +11,14 @@ export default class ContentBlock extends React.Component {
           -----------------------------
         </p>
         <div className="content-wrapper">
+          <img className="content-image" src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/1382809_10152484059162334_1836528230_n.jpg?oh=534567ea3f02a27b184fec1aeb085bf2&oe=5A0D3103"></img>
           <p className="content-head"> Welcome To Our New Site! </p>
           <p className="content-body">
             This is our new site, lorem ipsum and crap just trying to make this rubbish
             across a few lines so I can get some column shit sorted, you know what Im
             throwing down? this is skitzing out because the bloody syntax highlighting
             thinks this is all inside some quotations. Now its not though because Ive
-            had to sacrifice grammar for compiling
+            had to sacrifice grammar for compiling.
           </p>
         </div>
         <p id = "block-seperator">
