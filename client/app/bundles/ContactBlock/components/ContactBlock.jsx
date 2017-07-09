0eb2f8b4a70529391c6019c8c71c0a84110cf2ce
wckdwrld <z3484290@zmail.unsw.edu.au>
2017-07-10 08:24:42 +1000

    Committing before branch

--- client/app/bundles/ContactBlock/components/ContactBlock.jsx
+++ client/app/bundles/ContactBlock/components/ContactBlock.jsx
@@ -10,11 +10,33 @@ export default class ContactBlock extends React.Component {
       <div className = "contact-block">
         <div className="contact-wrapper">
           <div className="contact-form-left">
+            <p className="contact-head">
+              PODCAST
+            </p>
+            <div className="contact-body">
+              For all podcast enquiries, including interview requests, clarifications ect.
+              <p className="contact-body-email">
+                podcast@soap.productions
+              </p>
+              <p className="contact-body-phone">
+                +61 432 061 92
+              </p>
+            </div>
           </div>
-          <p id = "vertical-block-seperator">
-            -----------------------------
-          </p>
           <div className="contact-form-right">
+            <p className="contact-head">
+              HIRE US
+            </p>
+            <div className="contact-body">
+              Let's work together! Here at SOAP we aim to provide high quality content 
+              specialized for you!
+              <p className="contact-body-email">
+                work@soap.productions
+              </p>
+              <p className="contact-body-phone">
+                +61 432 061 92
+              </p>
+            </div>
           </div>
         </div>
       </div>
